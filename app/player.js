10acd87fca19e53c5a959714309af9ff2839f26a
James Prance <j.prance@gmx.com>
2017-10-16 17:28:55 +0100

    Fixed volume, seek(partially), shuffle and loop mpris events

--- app/player.js
+++ app/player.js
@@ -35,6 +35,7 @@ const Player = function(mainWindow, configPath) {
     player.on('playpause', function () {
         mainWindow.webContents.executeJavaScript('dzPlayer.control.togglePause();');
         update();
+	console.log('\'playpause pressed\'');
     });
 
     player.on('previous', function () {
@@ -48,32 +49,58 @@ const Player = function(mainWindow, configPath) {
         update();
     });
 
-    player.on('seek', function (event, value) {
-      // missing coef
-      console.log(value);
-    //   mainWindow.webContents.executeJavaScript('dzPlayer.control.seek(0..1);');
-    //   update();
-    });
+    player.on('seek', function(obj) {
+	console.log('seek pressed');
+        // ref: https://specifications.freedesktop.org/mpris-spec/latest/Player_Interface.html#Method:Seek
+	// implemented according to spec, due to differing functionality in the wild
+	// all times in microseconds
+	// works with playerctl, not gnome-shell-mediaplayer-indicator
 
-    player.on('position', function (event, value) {
-        var p = (value/200).toFixed(2);
-        console.log(p);
-        mainWindow.webContents.executeJavaScript('dzPlayer.control.seek('+p+');');
+	var seekFraction = (obj['position']/player.songDuration).toFixed(2);
+	if(seekFraction > 1) {
+	    mainWindow.webContents.executeJavaScript('dzPlayer.playTrackAtIndex(dzPlayer.getIndexSong()+1);');
+	} else if (seekFraction < 0) {
+	    mainWindow.webContents.executeJavaScript('dzPlayer.playTrackAtIndex(dzPlayer.getIndexSong()-1);');
+	} else {
+            mainWindow.webContents.executeJavaScript('dzPlayer.control.seek('+seekFraction+');');
+	}
         update();
     });
 
-    player.on('shuffle', function (event,value) {
-	   //  // missing value
-	   console.log(value);
-	   //  mainWindow.webContents.executeJavaScript('dzPlayer.control.shuffle(true|false);');
-	   //  update();
+    player.on('position', function(value,x) {
+	console.log('position pressed');
+        //var p = (value/200).toFixed(2);
+        //console.log(p);
+        //mainWindow.webContents.executeJavaScript('dzPlayer.control.seek('+p+');');
+        update();
     });
 
-    player.on('volume', function (event,value) {
-    //     // missing value
-	   console.log(value);
-    //     mainWindow.webContents.executeJavaScript('dzPlayer.control.setVolume(0..1);');
-    //     update();
+    player.on('shuffle', function(value) {
+	console.log('\'shuffle pressed\'');
+	mainWindow.webContents.executeJavaScript('dzPlayer.control.setShuffle('+value+');');
+	update();
+    });
+
+    player.on('loopStatus',function(value){
+	console.log('loop pressed: '+value);
+	switch(value) {
+	    case 'None':
+	    	mainWindow.webContents.executeJavaScript('dzPlayer.control.setRepeat(0);');
+	    	break;
+	    case 'Track':
+	    	mainWindow.webContents.executeJavaScript('dzPlayer.control.setRepeat(2);');
+		break;
+	    case 'Playlist':
+	    	mainWindow.webContents.executeJavaScript('dzPlayer.control.setRepeat(1);');
+		break;
+	}
+	update();
+    });
+
+    player.on('volume', function(value) {
+	console.log('volume pressed: '+value);
+        mainWindow.webContents.executeJavaScript('dzPlayer.control.setVolume('+value+');');
+        update();
     });
 
     ipcMain.on('context', function (event, values) {
@@ -103,6 +130,7 @@ const Player = function(mainWindow, configPath) {
                     }
 
 		    let url = 'https://e-cdns-images.dzcdn.net/images/cover/' + value.ALB_PICTURE + '/125x125.jpg';
+		    player.songDuration = value.DURATION * 1000000;
 
                     player.metadata = {
                         'mpris:trackid': value.SNG_ID,
@@ -148,6 +176,7 @@ const Player = function(mainWindow, configPath) {
                 }
             } else if (prop == "position") {
                 player.interfaces.player.emitSignal('Seeked', value  * 1000000);
+		player.position = value * 1000000;
             } else if (prop == "playbackStatus") {
                 // Compare two objects with JSON serialization -- See http://stackoverflow.com/a/1144249/2182996
                 if (context.playbackStatus === undefined || JSON.stringify(context.playbackStatus) != JSON.stringify(value)) {
